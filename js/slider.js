da1194424d211e2bd4489bc4133f5a89bc7e42c8
Soledad_2502 <example@example.com>
2021-12-06 20:03:39 -0300

    arreglando slider,footer y about

--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,9 @@
 let contador = 1;
 
 setInterval(function(){
-    document.getElementById('radio' + contador).checked =true;
+    document.getElementById('radio' + contador).checked = true;
     contador++;
     if(contador > 4){
         contador = 1;
     }
-},5000)
+},5000);
